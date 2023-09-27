d4aee50121db3def1b3481fc8c777c084e084d52
HassanRady <hassan.khaled.rady@gmail.com>
2023-09-27 17:12:53 +0200

    update

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ class Settings(BaseSettings):
     KAFKA_BOOTSTRAP_SERVERS: str
     KAFKA_CLEANED_TEXT_TOPIC: str
     KAFKA_KEYWORDS_TOPIC: str
+    KAFKA_AUTO_OFFSET_RESET: str
+    KAFKA_KEYWORDS_CONSUMER_GROUP: str
 
 
 @lru_cache()
--- main.py
+++ main.py
@@ -1,26 +1,66 @@
-from kafka import KafkaProducer, KafkaConsumer
-
+import sys
+from confluent_kafka import Consumer, Producer, KafkaError, KafkaException
 from config import settings
 from keywords_extractor import pipeline
-
 import json
 
-producer = KafkaProducer(bootstrap_servers=settings.KAFKA_BOOTSTRAP_SERVERS,
-                         value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
-consumer = KafkaConsumer(settings.KAFKA_CLEANED_TEXT_TOPIC,
-                         bootstrap_servers=settings.KAFKA_BOOTSTRAP_SERVERS,
-                         auto_offset_reset='latest',
-                         value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+def value_serializer(value):
+    return json.dumps(value).encode('utf-8') 
 
-try:
-    while True:
-        for message in consumer:
-            data = message.value
-            keywords = pipeline(data['text'])
-            if not keywords['output']:
-                continue
-            producer.send(settings.KAFKA_KEYWORDS_TOPIC, keywords)
-finally:
-    producer.close()
-    consumer.close()
+def value_deserializer(value):
+    return json.loads(value.decode('utf-8'))
+
+consumer_conf = {
+    'bootstrap.servers': settings.KAFKA_BOOTSTRAP_SERVERS,
+    'group.id': settings.KAFKA_KEYWORDS_CONSUMER_GROUP,
+    'auto.offset.reset': settings.KAFKA_AUTO_OFFSET_RESET,
+}
+
+producer_conf = {
+    'bootstrap.servers': settings.KAFKA_BOOTSTRAP_SERVERS,
+}
+
+consumer = Consumer(consumer_conf)
+consumer.subscribe([settings.KAFKA_CLEANED_TEXT_TOPIC])
+
+producer = Producer(producer_conf)
+
+
+def process_msg(msg):
+    data = value_deserializer(msg.value())
+    keywords = pipeline(data['text'])
+    if not keywords['output']:
+        return
+    return value_serializer(keywords)
+
+running = True
+
+def consume_produce_loop(consumer, consume_topics, produce_topic):
+    try:
+        consumer.subscribe(consume_topics)
+
+        while running:
+            msg = consumer.poll(timeout=1.0)
+            if msg is None: continue
+
+            if msg.error():
+                if msg.error().code() == KafkaError._PARTITION_EOF:
+                    sys.stderr.write('%% %s [%d] reached end at offset %d\n' %
+                                     (msg.topic(), msg.partition(), msg.offset()))
+                elif msg.error():
+                    raise KafkaException(msg.error())
+            else:
+                value = process_msg(msg)
+                if not value:
+                    continue
+                producer.produce(produce_topic, value)
+    finally:
+        consumer.close()
+        producer.flush()
+
+def shutdown():
+    running = False
+
+if __name__ == '__main__':
+    consume_produce_loop(consumer, [settings.KAFKA_CLEANED_TEXT_TOPIC], settings.KAFKA_KEYWORDS_TOPIC)
